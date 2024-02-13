fc55255148b5b6f2ac8a2f31d58fb263887e71c4
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2024-02-13 15:25:37 +0100

    cghfh,ju

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
   CharacterController controller;
+  Animator animator;
   Vector2 inputVector;
 
   [SerializeField]
@@ -14,16 +15,28 @@ public class PlayerController : MonoBehaviour
   void Start()
   {
     controller = GetComponent<CharacterController>();
+    animator = GetComponent<Animator>();
   }
 
   void Update()
   {
-    Vector3 movement = new (
-      inputVector.x,
-      0,
-      inputVector.y
-    );
-    
+    Vector3 movement = 
+        Camera.main.transform.right * inputVector.x
+      + Camera.main.transform.forward * inputVector.y;
+
+    if (movement.magnitude > 0)
+    {
+      movement.y = 0;
+      movement = movement.normalized;
+
+      transform.forward = movement;
+      animator.SetBool("walk", true);
+    }
+    else
+    {
+      animator.SetBool("walk", false);
+    }
+
     movement = movement * speed * Time.deltaTime;
     controller.Move(movement);
   }
